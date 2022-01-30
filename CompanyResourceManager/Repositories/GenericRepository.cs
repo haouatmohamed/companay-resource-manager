22ba0c19af54c83a9390996a1f3efd6a2fc1133b
Kiran Nandedkar <kiran.nandedkar@elementlogic.no>
2022-01-30 18:41:06 +0100

    just starting to analyze
    just starting to analyze

---
+++ CompanyResourceManager/Repositories/GenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CompanyResourceManager.DTOs;
+using CompanyResourceManager.Models;
+
+namespace CompanyResourceManager.Repositories
+{
+    public  class GenericRepository:IGenericInterface
+    {
+        public long AddEmployee(EmployeeDto employeeDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployeeById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Manager> GetAllEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetEmployeesBySalary(double salary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CompanyResourceManager/Repositories/IGenericInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CompanyResourceManager.DTOs;
+using CompanyResourceManager.Models;
+
+namespace CompanyResourceManager.Repositories
+{
+    public interface IGenericInterface
+    {
+        public long AddEmployee(EmployeeDto employeeDto);
+
+        public Employee GetEmployeeById(long id);
+
+        public IEnumerable<Manager> GetAllEmployee();
+
+        public void Save();
+
+        public IEnumerable<Employee> GetEmployeesBySalary(double salary);
+    }
+}
